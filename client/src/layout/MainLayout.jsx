f4d90a46a865e3eb0277e6a01016542ed6760b85
zzik <edin.base@outlook.com>
2022-08-29 11:41:06 +0200

    nt

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-import { /* Featured, */ /*Forum,*/ Hero } from "../components"
+import { Featured, Forum, Hero } from "../components"
 
 const MainLayout = () => {
   return (
     <div className="content">
       <Hero />
-      {/* <Forum /> */}
-      {/* <Featured /> */}
+      <Forum />
+      <Featured />
     </div>
   )
 }
